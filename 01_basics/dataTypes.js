1ba70acd9f244f937e2d53deb9b61f153268f768
Kalpana Nayak <156688653+Kalpana-Nayak@users.noreply.github.com>
2025-12-12 05:22:21 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all JS code as new version
+
+// alert(3 + 3) // can't be used here, as we are using nodejs not browser. only in console of a browser
+
+console.log(3 + 3
+     ) // code readability should be prioritize
+
+// Documentation : tc39 & MDN
+
+let name = "Kalpana" //String (written inside " ")
+let age = 21 //number
+let isLoggedIn = false //boolean (true/false)
+
+//number => 2 to the power 53
+//bigInt: used in stock market, trading
+
+//null => standalone value/ representation of an empty value
+//undefined => value not assigned
+
+//symbol => to find uniueness/ individuality (will be used during React)
+
+//object
+
+//typeof
+console.log(typeof age) //number
+console.log(typeof name) //string
+console.log(typeof isLoggedIn) //boolean
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
+
+
+
+
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,17 @@
+const accountId = 12345
+let accountEmail = "kalpananayak.11.33@gmail.com"
+var accountPassword = "helloworld"
+accountCity = "Sambalpur"
+let accountState
+
+// accountId = 23456 
+
+console.log(accountId);
+accountEmail = "kn@kk.com"
+accountPassword = "123"
+accountCity = "Hyderabad"
+
+/*don't use var, because of issue in block scope and functional scope*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
