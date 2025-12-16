a83757f5ee5e91bdc96eee8b712709fe1dc29521
Kalpana Nayak <156688653+Kalpana-Nayak@users.noreply.github.com>
2025-12-16 12:09:53 +0000

    map

--- 06_Iterations/high_order_loop.js
+++ 06_Iterations/high_order_loop.js
@@ -1,20 +1,40 @@
 // Some Array Specific loops
 // ["", "". ""] => strings [{}, {}, {}] => objects (. or [])
 
-// for of
-const arr = [10, 20, 30, 40, 50]
-for (const element of arr) {
-    console.log(element)
-}
+// // for of
+// const arr = [10, 20, 30, 40, 50]
+// for (const element of arr) {
+//     console.log(element)
+// }
 
-const greeting = "Hello, Good Morning"
-for (const greet of greeting) {
-    console.log(`Each char is ${greet}`)
-}
+// const greeting = "Hello, Good Morning"
+// for (const greet of greeting) {
+//     console.log(`Each char is ${greet}`)
+// }
 
 //maps
 const map = new Map()
 map.set("IN", "India")
-map.set("USA", "India")
-map.set("IN", "India")
+map.set("USA", "Unites States of America")
+map.set("JN", "Japan")
+map.set("IN", "India") // only takes unique value and in the exact same order
+// console.log(map)
+
+for (const key of map) {
+    console.log(key);
+}
+
+for (const [key, value] of map) { // de-structure of array
+    // console.log(key);
+    // console.log(value);
+    // console.log(key, ':-', value);
+}
+
+const myObject = {
+    "game1" : "NFS",
+    "game2" : "Spiderman"
+}
+for (const [key, value] of myObject) {   // myObject is not iterable
+    console.log(key, ':-', value);
+}
 
